97ecabe5b645ed55c4ee26797758888330d81800
Gustavo Ivan Vega Olvera <isc.givox@gmail.com>
2022-09-06 08:43:25 -0500

    Se agrego el layout de about

--- Proyecto/app/src/main/java/net/ivanvega/proyecto/RegisterActivity.kt
+++ Proyecto/app/src/main/java/net/ivanvega/proyecto/RegisterActivity.kt
@@ -1,9 +1,12 @@
 package net.ivanvega.proyecto
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var  txtEmail: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
