5de169566aadae3ff06d061ac604d39cb8706ae5
Gustavo Ivan Vega Olvera <isc.givox@gmail.com>
2022-09-07 09:02:20 -0500

    1) Se agrego paso de parametros a una actividad y se mando llamar y se ecupperararon 2) Se comenzo a mandar llamar actividades queb que pueden devolver algo

--- Proyecto/app/src/main/java/net/ivanvega/proyecto/LoginActivity.kt
+++ Proyecto/app/src/main/java/net/ivanvega/proyecto/LoginActivity.kt
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 
 class LoginActivity : AppCompatActivity() {
     lateinit var txtUser : EditText
     lateinit var txtPass : EditText
     lateinit var btnEntrar : Button
     lateinit var btnRegistrar : Button
+
 
 
 
@@ -27,12 +31,45 @@ class LoginActivity : AppCompatActivity() {
 
             //Para abrir otra activity se debe:
             //Crear un Intent de un Activity
+            /*
+            val intent_activity = Intent(
+                applicationContext, RegisterActivity::class.java
+            )*/
+
             val intent_activity = Intent(
                 applicationContext, RegisterActivity::class.java
             )
+            intent_activity.putExtra("par1",0)
+            intent_activity.putExtra("par2","Register")
+
+            /*val intent_activity = Intent(
+                applicationContext, RegisterActivity::class.java
+            ).apply {
+                this.putExtra("par1",0)
+                this.putExtra("par2","Register")
+            }*/
 
             //Abiri la activity
-            startActivity(intent_activity)
+            //startActivity(intent_activity)
+
+
+            //Para abrir una actividad que devuelve parametros  se debe construir los
+            //siguiente
+
+
+            //2) Usar el metodo starActivityFOrResult
+
+             val activityResultLauncher =
+                 registerForActivityResult(
+                ActivityResultContracts.StartActivityForResult(),
+                ActivityResultCallback {
+                    it.data
+                    it.resultCode
+                }
+                )
+
+
+            activityResultLauncher.launch(intent_activity)
 
         }
 
--- Proyecto/app/src/main/java/net/ivanvega/proyecto/RegisterActivity.kt
+++ Proyecto/app/src/main/java/net/ivanvega/proyecto/RegisterActivity.kt
@@ -2,6 +2,7 @@ package net.ivanvega.proyecto
 
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
@@ -11,6 +12,17 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_register)
+
+        //Recuperar parametros que se envian desde otra actividad
+        val titulo = intent.getStringExtra("par2")
+        val accion = intent.getIntExtra("par1",-1)
+
+        //propiedad title de laa activisdad
+        title = titulo
+        Toast.makeText(applicationContext,
+            "Accion: $accion", Toast.LENGTH_LONG).show()
+
+
     }
 
 }
