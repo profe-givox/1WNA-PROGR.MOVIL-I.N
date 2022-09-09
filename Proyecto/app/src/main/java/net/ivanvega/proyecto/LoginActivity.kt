123515d73557a6192e2cef98ad405279fa79d317
Gustavo Ivan Vega Olvera <isc.givox@gmail.com>
2022-09-09 08:24:49 -0500

    1) Se mando llamar una activity que devuelve un resultado 2) Se recupero el resultado

--- Proyecto/app/src/main/java/net/ivanvega/proyecto/LoginActivity.kt
+++ Proyecto/app/src/main/java/net/ivanvega/proyecto/LoginActivity.kt
@@ -19,24 +19,30 @@ class LoginActivity : AppCompatActivity() {
     lateinit var btnRegistrar : Button
 
     //Objeto que permite mandar llamar una actividad que devolvera un resultado
+    //La activiad que se invoca esta dentro de la misma App
     lateinit var  activityResultLauncher : ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //Se registra el tipo de actividad que se invocara(contrato),
+        // y se configurara la funcion callback que se dispara cuando
+        // cuando la actividad que se mando llamar devuelve un resultado
+        // El registro devuelve un ActivityResultLaucher , que es el ibjeto que permitira mandar llamar una actividad
         activityResultLauncher =
             registerForActivityResult(
                 ActivityResultContracts.StartActivityForResult(),
                 ActivityResultCallback {
+                    //el atributo it contiene el resultado y los datos devueltos
                     if( it.resultCode== RESULT_OK){
-                        txtUser.text = Editable.Factory.getInstance().newEditable(it?.data?.getStringExtra("email"))
+                        //txtUser.text = Editable.Factory.getInstance().newEditable(it?.data?.getStringExtra("email"))
                         val email =  it?.data?.getStringExtra("email")
                          //it.data.getStringExtra("name")
                         Toast.makeText(applicationContext,
                             "email: ${email}", Toast.LENGTH_LONG).show()
 
-
+                        txtUser.setText(email)
                     }
 
                 }
@@ -74,11 +80,8 @@ class LoginActivity : AppCompatActivity() {
             //startActivity(intent_activity)
 
 
-            //Para abrir una actividad que devuelve parametros  se debe construir los
-            //siguiente
-
-
-            //2) Usar el metodo
+            //2) actividad que devuelve parametros  se debe construir los
+            // lanzar la actividad que se desea abrir configurando previamente un intent
             activityResultLauncher.launch(intent_activity)
 
         }
