f5ddc81f67eff12dd528cd953d4a538b0e46614a
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2022-09-22 09:13:23 -0500

    SE CREO EL PROYECTO MIS RECYCLER VIEWS 1 Se agrego un Recycler View 2 Se desarrollo un adaptador para un recyclerr view

---
+++ MisRecyclersView/app/src/main/java/net/ivanvega/misrecyclersview/FlowerAdapter.kt
@@ -0,0 +1,36 @@
+package net.ivanvega.misrecyclersview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class FlowerAdapter (val dataset : Array<String>) : RecyclerView.Adapter<FlowerAdapter.FlowerViewHolder> () {
+    class FlowerViewHolder (view : View) : RecyclerView.ViewHolder(view) {
+        val flowerTextView : TextView
+        init {
+            flowerTextView = view.findViewById(R.id.flower_text)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlowerViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_flower_item, parent, false)
+
+        return FlowerViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: FlowerViewHolder, position: Int) {
+        holder.flowerTextView.text = dataset[position]
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+
+
+}
+
+
